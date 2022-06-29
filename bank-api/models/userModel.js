f6ffb4b8ab926cd1a1074892f0bdeb79df9985fe
Andrew Neilson <niandun87@gmail.com>
2022-06-28 23:33:32 -0500

    added password encryption

--- bank-api/models/userModel.js
+++ bank-api/models/userModel.js
@@ -30,7 +30,17 @@ const userSchema = mongoose.Schema(
     }
 
 
-)
+);
+
+//call a function before saving. It will be a middleware that has a next in it so after the operation is done it will move to the next one. 
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password=await bcrypt.hash(this.password, salt);
+})
 
 const User = mongoose.model('User', userSchema);
 
