e969d860c93f3caf891715501cac90f8d4ac6c05
Andrew Neilson <niandun87@gmail.com>
2022-07-01 21:55:28 -0500

    connected front end registration function to backend API

--- bank-client/src/components/Login.js
+++ bank-client/src/components/Login.js
@@ -1,87 +1,101 @@
 import MainScreen from "./MainScreen";
-import {useState } from 'react';
+import { useState } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
 // import {LinkContainer} from 'react-router-bootstrap'
-import Link from 'react-router-dom'
+import Link from "react-router-dom";
 import axios, { AxiosError } from "axios";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(false);
-    const [loading, setLoading] = useState(false);
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(email, password);
 
-    const submitHandler= async (e) => {
-        e.preventDefault();
-        console.log(email, password);
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      setLoading(true);
 
-        try{
-            const config = {
-                headers: {
-                    'Content-type': "application/json"
-                }
-            }
-            setLoading(true)
+      const { data } = await axios.post(
+        "/api/users/login",
+        {
+          email: email,
+          password: password,
+        },
+        config
+      );
 
-            const { data } = await axios.post('/api/users/login', {
-                email: email, password: password
-            }, config)
+      console.log(data);
 
-            console.log(data)
+      localStorage.setItem("userInfo", JSON.stringify(data));
 
-            localStorage.setItem('userInfo', JSON.stringify(data));
-
-        setLoading(false)
-        } catch (error) {
-            setError(error.response.data.message);
-        }
-    };
-
-    const sayHello = async() => {
-        const response = await fetch('/api/hello')
-        const data = await response.json()
-        console.log(data)
+      setLoading(false);
+    } catch (error) {
+      setError(error.response.data.message);
     }
+  };
 
-    const trackUser = async() => {
-        const response = await fetch('/api/users/track')
-        const data = await response.json()
-        console.log(data)
-    }
+  const sayHello = async () => {
+    const response = await fetch("/api/hello");
+    const data = await response.json();
+    console.log(data);
+  };
 
-    // const sayHello = () => {
-    //     fetch('/api/hello')
-    //     .then(res => res.json())
-    //     .then(data => console.log(data))
-    // }
-    
+  const trackUser = async () => {
+    const response = await fetch("/api/users/track");
+    const data = await response.json();
+    console.log(data);
+  };
+
+  // const sayHello = () => {
+  //     fetch('/api/hello')
+  //     .then(res => res.json())
+  //     .then(data => console.log(data))
+  // }
+
   return (
-    
     <MainScreen title="Login">
-        <div className='loginContainer'>
-      <Form onSubmit={submitHandler}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" value={email} placeholder="Enter email" onChange={(e) => setEmail(e.target.value)}/>
-          <Form.Text className="text-muted">
-            If you haven't signed up yet, please register here.
-          </Form.Text>
-        </Form.Group>
+      <div className="loginContainer">
+        <Form onSubmit={submitHandler}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              value={email}
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Form.Text className="text-muted">
+              If you haven't signed up yet, please register here.
+            </Form.Text>
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form> 
-      <button onClick={trackUser} text="hello">hello</button> 
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        <button onClick={trackUser} text="hello">
+          hello
+        </button>
       </div>
-  </MainScreen>
-  
-    
-)};
+    </MainScreen>
+  );
+};
 
 export default Login;
--- bank-client/src/components/Signup.js
+++ bank-client/src/components/Signup.js
@@ -1,10 +1,108 @@
-import MainScreen from './MainScreen'
-
+import MainScreen from "./MainScreen";
+import { useState } from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import ErrorMessage from './ErrorMessage'
+import axios from 'axios';
 const Signup = () => {
 
-    return (
-        <MainScreen title="CREATE AN ACCOUNT"></MainScreen> 
-    )
-}
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
 
-export default Signup
+    const submitHandler = async (e) => {
+        e.preventDefault();
+
+        if (password !== confirmPassword) {
+            setMessage('Password not entered correctly')
+        } else {
+            try {
+                const config = {
+                  headers: {
+                    "Content-type": "application/json",
+                  },
+                };
+                setLoading(true);
+          
+                const { data } = await axios.post(
+                  "/api/users",
+                  {
+                    name: name,
+                    email: email,
+                    password: password,
+                  },
+                  config
+                );
+          
+                console.log(data);
+          
+                localStorage.setItem("userInfo", JSON.stringify(data));
+          
+                setLoading(false);
+              } catch (error) {
+                setError(error.response.data.message);
+              }
+
+        }
+        
+    }
+
+  return (
+    <MainScreen title="CREATE AN ACCOUNT">
+      <div className="loginContainer">
+          {message && <ErrorMessage variant='danger'>{message}</ErrorMessage>}
+        <Form onSubmit={submitHandler}>
+          <Form.Group className="mb-3" controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              value={name}
+              placeholder="Enter your name"
+              onChange={(e) => setName(e.target.value)}
+            />
+            </Form.Group>
+             <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              value={email}
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            
+            <Form.Text className="text-muted">
+              If you haven't signed up yet, please register here.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="confirmPassword">
+            <Form.Label>Confirm password</Form.Label>
+            <Form.Control
+              type="password"
+              value={confirmPassword}
+              placeholder="Confirm password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            </Form.Group>
+          <Button variant="primary" type="submit">
+            Register
+          </Button>
+        </Form>
+      </div>
+    </MainScreen>
+  );
+};
+
+export default Signup;
