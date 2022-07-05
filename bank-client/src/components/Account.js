9d058823bb394e0e0a9994a97345a39a8f297c20
Andrew Neilson <niandun87@gmail.com>
2022-07-05 17:26:05 -0500

    added update balance function to account page

--- bank-client/src/components/Account.js
+++ bank-client/src/components/Account.js
@@ -1,11 +1,35 @@
 import MainScreen from "./MainScreen";
+import { useState, useEffect } from 'react'
+import axios from "axios";
 
 const Account = () => {
+
+    const [userBalance, setUserBalance] = useState(0)
+
+    useEffect( () => {
+
+        getBalance()
+
+    }, [])
+
+    const getUser = () => {
+        const userData = localStorage.getItem('userInfo')
+        const parseData = JSON.parse(userData)
+        return parseData
+    }
+
+    const getBalance = async () => {
+        const email = getUser()['email']
+        const data = await axios('/api/users/currentbalance', {params: {email: email}})
+        console.log(data)
+        //Set user balance with data
+        setUserBalance(data.data['balance'])
+    }
 
     return (
         <MainScreen title="Your Account Summary">
             <div>
-                Your current balance is 
+                Your current balance is ${userBalance.toFixed(2)} 
             </div>
         </MainScreen>
     )
